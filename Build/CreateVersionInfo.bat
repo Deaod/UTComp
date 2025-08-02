3db74e41c057d86ab571c4938d6890a191a72116
Lukas Bagaric <deaeod@gmail.com>
2025-08-02 22:13:31 +0200

    Build System Fixes

--- Build/CreateVersionInfo.bat
+++ Build/CreateVersionInfo.bat
@@ -5,9 +5,9 @@ set INFO_FILE="%BUILD_DIR%Classes/VersionInfo.uc"
 if not exist "%BUILD_DIR%Classes" mkdir "%BUILD_DIR%Classes"
 
 @if NOT EXIST %INFO_FILE% (
-	@echo PackageBaseName=%1
-	@echo PackageVersion=%2
-	@echo PackageName=%3
+	@echo PackageBaseName="%1"
+	@echo PackageVersion="%2"
+	@echo PackageName="%3"
 
 	 echo class VersionInfo extends Info;>%INFO_FILE%
 	@echo.>>%INFO_FILE%
@@ -16,9 +16,9 @@ if not exist "%BUILD_DIR%Classes" mkdir "%BUILD_DIR%Classes"
 	@echo var string PackageName;>>%INFO_FILE%
 	@echo.>>%INFO_FILE%
 	@echo defaultproperties {>>%INFO_FILE%
-	@echo     PackageBaseName=%1>>%INFO_FILE%
-	@echo     PackageVersion=%2>>%INFO_FILE%
-	@echo     PackageName=%3>>%INFO_FILE%
+	@echo     PackageBaseName="%1">>%INFO_FILE%
+	@echo     PackageVersion="%2">>%INFO_FILE%
+	@echo     PackageName="%3">>%INFO_FILE%
 	@echo }>>%INFO_FILE%
 	@echo.>>%INFO_FILE%
 )
